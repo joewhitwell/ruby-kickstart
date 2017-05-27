ad95e9e58407e7bebd5e5d0693b46ada7450c095
Joe Whitwell <joewhitwell@hotmail.com>
2017-05-27 19:47:32 +0000

    Fairly simple, once who discuver the each_cons method

--- session2/challenge/8_array.rb
+++ session2/challenge/8_array.rb
@@ -5,3 +5,13 @@
 # got_three? ['a', 'a', 'b']  # => false
 # got_three? ['a', 'a', 'a']  # => true
 # got_three? [1, 2, 1, 1]     # => false
+
+# the in a row is crucial here,  use a count function and then if statement
+# if n exists 3 or more times, maybe use the counts method, 
+
+def got_three? (elements) # applied to elements
+    elements.each_cons 3 do |a, b, c| #where there are 3 consecutive elements, 
+        return true if a == b && a == c #they are all equal
+    end
+false
+end

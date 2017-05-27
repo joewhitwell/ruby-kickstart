77c5d801c969cfb8fe1275c72e57b7efafcd2dc6
Joe Whitwell <joewhitwell@hotmail.com>
2017-05-27 12:17:01 +0000

    Need further consideratino of the character elements

--- session1/challenge/7_string.rb
+++ session1/challenge/7_string.rb
@@ -4,5 +4,15 @@
 # pirates_say_arrrrrrrrr("Katy Perry is on the radio!")   # => "rya"
 # pirates_say_arrrrrrrrr("Pirates say arrrrrrrrr")        # => "arrrrrrrr"
 
-def pirates_say_arrrrrrrrr(string)
+    def pirates_say_arrrrrrrrr(string) 
+  to_return = "" # return a string
+  add_next = false 
+  string.size.times do |index| #iterate upwards adding to the index for each look
+    current_char = string[index,1] # the present character is index 1 ont he string
+    to_return << current_char if add_next #move to the next character
+    add_next = (current_char == "r" || current_char == "R") # only add if the current character is an upper or lower case 
+  end
+  to_return
 end
+    
+    

c744cab4ed87914eca4df5dc064aac751c20b786
Joe Whitwell <joewhitwell@hotmail.com>
2017-05-28 19:50:54 +0000

    Further array creation

--- session3/challenge/4_hashes.rb
+++ session3/challenge/4_hashes.rb
@@ -5,3 +5,10 @@
 # There will be no punctuation in the strings.
 #
 # first_pos "The dog and the cat and the cow" # => {"The" => 0, "dog" => 1, "and" => 2, "the" => 3, "cat" => 4, "cow" => 7}
+
+def first_pos(str) #method that takes a string
+    to_return = {} # which create an empty hash as denoted by curly brackets
+    str.split.each_with_index { |word, index| to_return [word] ||= index } # split into  array of words, along with each index, 
+    # those run through block, which inserts but does not replace 
+    to_return
+end

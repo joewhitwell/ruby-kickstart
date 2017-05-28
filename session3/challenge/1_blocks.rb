cd0d59acf83bf9cf87fe656636d9165b48388914
Joe Whitwell <joewhitwell@hotmail.com>
2017-05-28 18:49:53 +0000

    First time making sense of *args

--- session3/challenge/1_blocks.rb
+++ session3/challenge/1_blocks.rb
@@ -3,3 +3,7 @@
 #
 # reverse_map(1, 2, 3) { |i| i * 2 }      # => [6, 4, 2]
 
+def reverse_map(*args, &block) #method takes arguemtns made into an array 
+  args.reverse.map(&block) #those arguments are reverse and mapped out into a new array 
+end
+

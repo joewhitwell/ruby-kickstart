7a956b5b808c0741d915a65cbc930aff672e5fa0
Joe Whitwell <joewhitwell@hotmail.com>
2017-05-28 19:19:57 +0000

    Further hashing

--- session3/challenge/2_hashes.rb
+++ session3/challenge/2_hashes.rb
@@ -8,3 +8,17 @@
 # staircase 4  # => {1 => [], 3 => [2]}
 # staircase 5  # => {1 => [], 3 => [2], 5 =>[2, 4]}
 
+# initial thoughts non negative integer eg num, 
+#hash denoted with curly brackers
+
+def staircase(n) #introduce the method staircase which take n
+  to_return = {} # will fill  hash and return 
+  1.upto n do |crnt_size| #starting from 1 executes block upto n
+    next if crnt_size.even? # skipping even numbers
+    all   = Array.new(crnt_size) { |i| i + 1 }  #creates an array of 1 to current size
+    evens = all.select { |i| i.even? } # dselects all even interviews
+    to_return[crnt_size] = evens # adds a mapping from current size to evens
+  
+  end
+  to_return
+end

11b4d267f6b90974cd4b1d9d94d0b9839005f7e5
Joe Whitwell <joewhitwell@hotmail.com>
2017-05-28 19:35:26 +0000

    New use of .downcase method in a block

--- session3/challenge/3_hashes.rb
+++ session3/challenge/3_hashes.rb
@@ -6,3 +6,13 @@
 #
 # Example:
 # word_count "The dog and the cat" # => {"the" => 2, "dog" => 1, "and" => 1, "cat" => 1}
+
+
+def word_count(str) #method is applied to the string 
+    words = Hash.new { 0 } # if word is not in the hash, return 0
+    str.split.each { |word| words [word.downcase] += 1} #create an array of words, 
+    # each runs block on each word lowering the case
+    words
+end
+
+    
